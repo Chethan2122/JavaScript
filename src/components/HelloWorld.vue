f93ff1f293acfc0d80c1af29eb78f3b92cd356d9
chethan <pondurichethan@ngpwebsmart.com>
2022-09-07 11:19:40 +0530

    ss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,169 +1,121 @@
 <template>
-  <v-form>
-    <v-text-field
-      v-model="firstname"
-      label="firstname"
-      placeholder="firstname"
-      :rules="firstnamerule"
-      required
-      >
-    </v-text-field>
-    <v-text-field
-      v-model="lastname"
-      label="lastname"
-      placeholder="lastname"
-      :rules="lastnamerule"
-      required
-      >
-    </v-text-field>
-    <v-text-field
-      v-model="email"
-      label="email"
-      placeholder="email"
-      :rules="emailrule"
-      required
-    >
-    </v-text-field>
-    <v-text-field
-      v-model="mobilenumber"
-      label="mobilenumber"
-      placeholder="mobilenumber"
-      :rules="numberrule"
-      required
-      >
-    </v-text-field>
-    <v-select
-      v-model="city"
-      :items="item"
-      dense
-      :rules="[v => !!v || 'Item is required']"
-      label="City"
-      placeholder="select city"
-      required
-    ></v-select>
-    <h3>Gender</h3>
-    <v-radio-group
-      v-model="radio"
-      column
-    >
-    <v-radio
-      label="male"
-      value="primary"
-    ></v-radio>
-    <v-radio
-      label="female"
-      value="secondary"
-    ></v-radio>
-    <v-radio
-      label="Other"
-      value="tertiary"
-    ></v-radio>
-    </v-radio-group>
-    <h3>Interests</h3>
-    <v-checkbox
-      v-model="Select"
-      label="Photography"
-      Value="Photography"
-      ></v-checkbox>
-    <v-checkbox
-      v-model="Select"
-      label="Travelling"
-      Value="Travelling"
-      ></v-checkbox>
-    <v-checkbox
-      v-model="Select"
-      label="Music"
-      Value="Music"
-    ></v-checkbox>
-    <v-checkbox
-      v-model="select"
-      label="None"
-      value="None"
-    ></v-checkbox>
-    <v-btn
-     :disabled="!valid"
-      color="black"
-      @click="validate"
-    >
-    Validate
-    </v-btn>
-    
-  
-  
-  </v-form>
-  </template>
-  <script>
-    export default {
-      data:() => ({
-        valid : true,
-        firstname : "",
-        firstnamerule:[
-          firstname => !!firstname||'Name is required'
-        ],
-        lastname : "",
-        lastnamerule:[
-          lastname => !!lastname||'Name is required'
-        ],
-        Email : "",
-        emailrule:[
-          email => !!email||'Email is required'
-        ],
-        mobilenumber : "",
-        numberrule:[
-          mobilenumber => !!mobilenumber||'Email is required'
-        ],
-        select : null,
-        item:[
-          'Chennai',
+  <v-app>
+      <v-form
+     ref="form"
+     v-model="valid"
+     lazy-validation
+   >
+     <v-text-field
+       v-model="name"
+       :rules="nameRules"
+       label="Name"
+       required
+     ></v-text-field>
+     <v-text-field
+       v-model="email"
+       :rules="emailRules"
+       label="E-mail"
+       required
+     ></v-text-field>
+     <v-text-field
+       v-model="mobilenumber"
+       :rules="numberRules"
+       label="mobilenumber"
+       required
+     ></v-text-field>
+     <v-select
+       v-model="select"
+       :items="location"
+       :rules="[v => !!v || 'Location is required']"
+       label="location"
+       required
+     ></v-select>
+     <br>
+     <h3>Gender</h3>
+     <v-radio-group
+     v-model="gender"
+     column
+     >
+     <v-radio
+     label="male"
+     value="Male"
+     ></v-radio>
+     <v-radio
+     label="female"
+     value="Female"
+     ></v-radio>
+     <v-radio
+     label="other"
+     value="other"
+     ></v-radio>
+</v-radio-group>
+   <h2>Interests</h2>
+      <v-checkbox
+     v-model="Interests"
+       v-for="(cho) in choice"
+       :key="cho.id"
+       :label="cho.name"
+       :value="cho.name"
+       required
+     ></v-checkbox>
+     <v-text-field
+       v-model="others"
+       label="Mention if others"
+     ></v-text-field>
+   
+      <v-btn
+       :disabled="!valid"
+       color="success"
+       class="mr-4"
+       @click="validate"
+     >
+       Validate
+     </v-btn>
+   </v-form>
+    </v-app>
+</template><script>
+export default {
+     data: () => ({
+       valid: true,
+       name: '',
+       nameRules: [
+         name=>!!name||'Name is required',
+         v => /[a-zA-Z]/.test(v) || 'Name must be valid'
+       ],
+       email: '',
+       emailRules: [
+         email => !!email || 'E-mail is required',
+         v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+       ],
+       mobilenumber: '',
+       numberRules: [
+         mobilenumber=>!!mobilenumber||'Number is required',
+         v => /[0-9]/.test(v) || 'Number must be valid'
+       ],
+       select: null,
+       location: [
+       'Chennai',
           'Mumbai',
           'Banglore',
           'Hyderabad',
           'Delhi',
           'Kolkata',
           'Pune'
-          ],
-          city:"",
-          
-    }),
-    methods :{
-      validate () {
-        this.$refs.form.validate()
-      const arr = {
-        firstname : this.firstname,
-        lastname : this.lastname,
-        email : this.email,
-        gender :this.gender,
-        mobilenumber :this.mobilenumber,
-        checkbox: this.checkbox,
-        selected: this.city,
-        row: this.row
-      }
-      console.log(JSON.stringify(arr))
-      }
-    }
-  }
-    
-  </script>
-  <style lang="scss">
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-  
-  nav {
-    padding: 30px;
-  
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-  
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
-  }
-  </style>
-  
+       ],
+       choice: [
+      {id :1,name:'music'},{id:2,name:'photography'},{id:3,name:'sports'},{id:4,name:'others'}],
+      Interests:[]
+     }),
+     methods: {
+       validate () {
+         this.$refs.form.validate()
+         const arr = {
+           name : this.name,
+           email : this.email,
+           gender :this.gender,
+           Hobbies: this.Hobbies,
+           select: this.select,          }
+         console.log(JSON.stringify(arr))
+       }    }
+}
+</script>
